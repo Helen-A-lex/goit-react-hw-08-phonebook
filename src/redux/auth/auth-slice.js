af985cf821901f1a13b2ed17ca2384cf6ed015d1
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-07-17 19:14:13 +0200

    Update auth-slice.js

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -27,13 +27,17 @@ const authSlice = createSlice({
         state.token = null;
         state.isLoggedIn = false;
       })
+      .addCase(fetchCurrentUser.pending, state => {
+        state.isRefreshing = true;
+      })
 
       .addCase(fetchCurrentUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
+        state.isRefreshing = false;
       })
       .addCase(fetchCurrentUser.rejected, state => {
-        state.isRefreshing = true;
+        state.isRefreshing = false;
       }),
 });
 export const authReducer = authSlice.reducer;
